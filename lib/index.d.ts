55293a2e02e3c562d35136ed7313667efdf19d29
Peter Vizi <89486174+peter-vizi-pager@users.noreply.github.com>
2023-06-05 13:06:09 -0500

    fix: add type definition (#527)

---
+++ lib/index.d.ts
@@ -0,0 +1,52 @@
+declare module '@pager/minion' {
+
+    import { EventEmitter } from 'events';
+
+    export type Settings = {
+        arguments?: Record<string, unknown>
+        autoDelete?: boolean
+        autoStart?: boolean
+        deadLetterExchange?: string | boolean
+        debug?: boolean
+        durable?: boolean
+        exchangeName?: string
+        exchangeType?: string
+        exclusive?: boolean
+        key?: string
+        keys?: string[]
+        name?: string
+        prefetch?: number
+        queueMode?: string
+        rabbit?: unknown
+        rabbitUrl?: string
+        requeue?: boolean
+    }
+
+
+    // As documented in the amqp module:
+    // https://amqp-node.github.io/amqplib/channel_api.html#channel_consume
+    export type AmqpMessage = {
+        content: Buffer
+        fields: Object
+        properties: Object
+    }
+
+    export type HandlerFn = (message: string | any, metadata: AmqpMessage) => Promise<unknown>;
+
+    export type Publisher = (message: unknown, key: unknown, properties: unknown) => void;
+
+    export type Service = EventEmitter & {
+        connection: unknown
+        handle: (message: unknown, metadata: unknown) => Promise<unknown>
+        options: unknown
+        publish: Publisher
+        start: () => void
+    }
+
+    declare function createMinion(settings?: Settings, settingsOverride?: Settings): Publisher;
+
+    declare function createMinion(handler: HandlerFn, settingsOverride?: Settings): Service;
+
+    export = createMinion;
+
+};
